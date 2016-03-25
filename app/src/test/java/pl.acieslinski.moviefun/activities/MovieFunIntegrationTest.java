8f4cffc575302bd88b83b5a36ffe69e312042407
acieslinski <acieslinski@gmail.com>
2016-03-25 16:19:57 +0100

    mocking rx newThread method

--- app/src/test/java/pl.acieslinski.moviefun/activities/MovieFunIntegrationTest.java
+++ app/src/test/java/pl.acieslinski.moviefun/activities/MovieFunIntegrationTest.java
@@ -1,37 +1,42 @@
 package pl.acieslinski.moviefun.activities;
 
-import android.app.AlertDialog;
+import android.app.Activity;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.view.ViewPager;
-import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.FrameLayout;
 
 import org.apache.http.HttpRequest;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.tools.ant.taskdefs.condition.Http;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricGradleTestRunner;
-import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
 import org.robolectric.shadows.ShadowLog;
-import org.robolectric.shadows.ShadowProgressDialog;
 import org.robolectric.shadows.httpclient.FakeHttp;
 import org.robolectric.shadows.support.v4.SupportFragmentTestUtil;
 
+import java.net.URI;
+import java.util.concurrent.TimeUnit;
+
 import pl.acieslinski.moviefun.BuildConfig;
 import pl.acieslinski.moviefun.R;
-import pl.acieslinski.moviefun.fragments.SearchForm;
 import pl.acieslinski.moviefun.fragments.SearchList;
 import pl.acieslinski.moviefun.fragments.VideoList;
-import pl.acieslinski.moviefun.models.SearchEvent;
+import rx.Scheduler;
+import rx.Subscription;
+import rx.functions.Action0;
+import rx.internal.schedulers.ScheduledAction;
+import rx.plugins.RxJavaPlugins;
+import rx.plugins.RxJavaSchedulersHook;
 
 
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThat;
 
 /**
  * @author Arkadiusz Cieśliński 24.03.16.
@@ -53,23 +58,16 @@ public class MovieFunIntegrationTest  {
         mSearchList = new SearchList();
         mVideoList = new VideoList();
 
-        SupportFragmentTestUtil.startVisibleFragment(mSearchList);
-//        SupportFragmentTestUtil.startVisibleFragment(mVideoList);
+        SupportFragmentTestUtil.startFragment(mSearchList);
 
-        FrameLayout frameLayout = new FrameLayout(mMovieFun);
-        frameLayout.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
-                ViewGroup.LayoutParams.MATCH_PARENT));
-        frameLayout.setId(R.id.pageRight);
-        mMovieFun.mContainer.addView(frameLayout);
+        // Robolectric ViewPager issue with attaching fragments
+        attachVideoContainer(mMovieFun, mVideoList);
 
-        mMovieFun.getSupportFragmentManager().beginTransaction().add(R.id.pageRight, mVideoList).commit();
-        mMovieFun.getSupportFragmentManager().executePendingTransactions();
+        RxJavaPlugins.getInstance().registerSchedulersHook(new RobolectricRxJavaSchedulersHook());
     }
 
     @Test
     public void sendingSearchQueryTest() throws InterruptedException {
-
-
         EditText editText = (EditText) mSearchList.getView().findViewById(R.id.et_search);
         Button button = (Button) mSearchList.getView().findViewById(R.id.btn_search);
 
@@ -77,21 +75,72 @@ public class MovieFunIntegrationTest  {
         button.callOnClick();
 
         Robolectric.flushForegroundThreadScheduler();
-        Robolectric.flushBackgroundThreadScheduler();
-
-
-        Thread.sleep(60000);
 
         HttpRequest httpRequest = FakeHttp.getLatestSentHttpRequest();
 
         httpRequest.getParams();
     }
-}
 
-class MovieFunTest extends MovieFun {
+    /**
+     * Creates special container for the {@link VideoList} for the test purposes
+     */
+    private void attachVideoContainer(MovieFun movieFun, VideoList videoList) {
+        FrameLayout frameLayout = new FrameLayout(mMovieFun);
+        frameLayout.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT));
+        frameLayout.setId(R.id.pageRight);
+        movieFun.mContainer.addView(frameLayout);
 
+        movieFun.getSupportFragmentManager().beginTransaction().add(R.id.pageRight, videoList).
+                commit();
+        movieFun.getSupportFragmentManager().executePendingTransactions();
+    }
 
-    public void onEventMainThread(SearchEvent searchEvent) {
+    private class RobolectricRxJavaSchedulersHook extends RxJavaSchedulersHook {
+        /**
+         * Returns {@link Robolectric#getBackgroundThreadScheduler()} instead of the
+         * {@link rx.schedulers.Schedulers#newThread()}
+         */
+        @Override
+        public Scheduler getNewThreadScheduler() {
+            return new RobolectricRxScheduler();
+        }
+    }
 
+    private class RobolectricRxScheduler extends Scheduler {
+        boolean isUnsubscribed;
+
+        @Override
+        public Worker createWorker() {
+            return new Worker() {
+                @Override
+                public Subscription schedule(Action0 action) {
+                    ScheduledAction scheduledAction = new ScheduledAction(action);
+
+                    Robolectric.getBackgroundThreadScheduler().post(() -> {
+                        scheduledAction.run();
+                        scheduledAction.unsubscribe();
+                    });
+
+                    return scheduledAction;
+                }
+
+                @Override
+                public Subscription schedule(Action0 action, long delayTime, TimeUnit unit) {
+                    return null;
+                }
+
+                @Override
+                public void unsubscribe() {
+                    isUnsubscribed = true;
+                }
+
+                @Override
+                public boolean isUnsubscribed() {
+                    return isUnsubscribed;
+                }
+            };
+        }
     }
 }
+
